7dbcf503288b2c58de9ecdafcc24411a618db8ca
SYED MD SAIFI HASSAN <38734037+syedmdsaifihassan@users.noreply.github.com>
2021-03-15 23:35:28 +0530

    Create loop_practice.java

---
+++ Java/Core Java/loop_practice.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class loop_practice {
+    public static void main(String[] args) {
+        // Question 1
+        // for(int i=4;i>0;i--){
+        //     for(int j=0;j<i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.printf("\n");
+        // }
+
+        // Question 2
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the no. of inputs: ");
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // while(n>=0){
+        //     if(n%2==0){
+        //         sum += n;
+        //     }
+        //     n--;
+        // }
+        // System.out.println("Sum of Even Numbers is: "+sum);
+
+        // Question 3
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("\'Table\'Enter the no.: ");
+        // int n = sc.nextInt();
+        // for (int i=1;i<=10;i++){
+        //     System.out.println(n+"*"+i+"="+n*i);
+        // }
+
+        // Question 4
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("\'Reverse Table\'Enter the no.: ");
+        // int n = sc.nextInt();
+        // for (int i=10;i!=0;i--){
+        //     System.out.println(n+"*"+i+"="+n*i);
+        // }
+
+        // Question 5
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("\'Factorial\'Enter the no.: ");
+        // int n = sc.nextInt();
+        // int mul = 1;
+        // for(int i=n;i!=0;i--){
+        //     mul = mul*i;
+        // }
+        // System.out.println("The factorial of "+n+" is equal to "+mul);
+
+        // Question 6
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("\'Factorial\'Enter the no.: ");
+        // int n = sc.nextInt();
+        // int mul = 1;
+        // int i = n;
+        // while(i!=0){
+        //     mul *= i;
+        //     i--;
+        // }
+        // System.out.println("The factorial of "+n+" is equal to "+mul);
+
+        // Question 7
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("\'Sum of table of a number\'Enter the no.: ");
+        // int n = sc.nextInt();
+        // int i = 10;
+        // int sum = 0;
+        // while(i!=0){
+        //     sum = sum + (n*i);
+        //     i--;
+        // }
+        // System.out.println(sum);
+
+        // Question 8
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("\'do while\'Enter the no.: ");
+        // int i = sc.nextInt();
+        // do {
+        //     System.out.println(i+" do while loop");
+        //     i++;
+        // } while (i<10);
+
+        // Question 9
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("\'Question 2 using for loop\'Enter the no. of inputs: ");
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // for(int i=2;i<=n;i++){
+        //     if(i%2==0){
+        //         sum += i;
+        //     }
+        // }
+        // System.out.println("Sum of even no.s: "+sum);
+
+    }
+}
